bbc958526d0a159a7814ad2c37a9a4f1f747b3bc
ben20262 <ben20262@aim.com>
2020-04-27 20:17:16 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
+	return (
+		<div>
+			<h1>Directors Page</h1>
+			{directors.map((director, index) => {
+				return (
+					<div key={`director-${index}`}>
+						<h3>{director.name}</h3>
+						<ul>
+							{director.movies.map((movie, mIndex) => {
+								return <li key={`movie-${mIndex}`}>{movie}</li>
+							})}
+						</ul>
+					</div>
+				)
+			})}
+		</div>
+	);
 }
 
 export default Directors
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,20 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+		{movies.map((movie, index) => {
+			return (
+				<div key={`movie-${index}`}>
+					<h3>{movie.title}</h3>
+					<p>Runtime: {movie.time}</p>
+					<ul>
+					{movie.genres.map((genre, gIndex) => {
+						return <li key={`genre-${gIndex}`}>{genre}</li>
+					})}
+					</ul>
+				</div>
+			)
+		})}
     </div>
   );
 };
