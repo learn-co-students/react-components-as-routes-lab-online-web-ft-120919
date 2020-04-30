a6d058ddad2bde37c736943cce96c5728493c379
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-04-30 12:07:20 -0700

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,31 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink
+        to="/"
+        exact
+        // style={link}
+        activeStyle={{ background: 'grey' }}
+      >Home</NavLink>
+      <NavLink
+        to="/movies"
+        exact
+        // style={link}
+        activeStyle={{ background: 'grey' }}
+      >Movies</NavLink>
+      <NavLink
+        to="/directors"
+        exact
+        // style={link}
+        activeStyle={{ background: 'grey' }}
+      >Directors</NavLink>
+      <NavLink
+        to="/actors"
+        exact
+        // style={link}
+        activeStyle={{ background: 'grey' }}
+      >Actors</NavLink>
     </div>
   );
 };
